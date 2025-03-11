fa1374c6ae4f6bd1611a6bc90762c221df636b1d
Dastan Oskonbaev <dastiw1910@gmail.com>
2025-03-11 15:14:00 +0600

    Login

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from apps.users.views import register_view
+from apps.users.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,7 @@
+
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from apps.users.forms import CustomUserCreationForm
@@ -13,5 +17,32 @@ def register_view(request):
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                messages.success(request, "OK")
+                print("OK")
+                return redirect('/')
+            else:
+                messages.error(request, "ERROR")
+                print("ERROR")
+        else:
+            messages.error(request, "ERROR")
+            print("ERROR")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "OK")
+    return redirect('/')
 
 
